138b36d412993ed57e3ca158c7fe0617fb570695
Aiko Muraishi <blllue.sea.blllue@gmail.com>
2020-10-11 13:08:19 +0900

    tensorflow demo

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import action from "./action.js";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -8,8 +8,7 @@ export default new Vuex.Store({
   },
   mutations: {
   },
-  actions: {
-  },
+  actions: action,
   modules: {
   }
 })
